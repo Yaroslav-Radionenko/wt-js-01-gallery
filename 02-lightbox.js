6e8746a94a5d92da20dc981cdf70a96a447a6803
Yaroslav-Radionenko <radionenkoyaroslav.stud@gmail.com>
2025-03-13 20:49:49 +0100

    test zavdana' 1

--- 02-lightbox.js
+++ 02-lightbox.js
@@ -1,4 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Ваш код
+<li class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+        <img
+            class="gallery__image"
+            src="small-image.jpg"
+            data-source="large-image.jpg"
+            alt="Image description"
+        />
+    </a>
+</li>
 
 console.log(galleryItems);
