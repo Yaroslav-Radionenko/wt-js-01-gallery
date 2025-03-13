1394999de665a875130349fd424984546ccf555c
Yaroslav-Radionenko <radionenkoyaroslav.stud@gmail.com>
2025-03-13 21:34:37 +0100

    test task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,14 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Ваш код
-<li class="gallery__item">
-    <a class="gallery__link" href="large-image.jpg">
+
+import { galleryItems } from './gallery-items.js';
+
+// це gallery DOM element
+const galleryContainer = document.querySelector(".gallery");
+
+// створення (відтворення) картки 
+function createElementMarkup(markup) {
+    return markup.map(({ description, preview, original }) => {
+
+        return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
         <img
             class="gallery__image"
-            src="small-image.jpg"
-            data-source="large-image.jpg"
-            alt="Image description"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
         />
     </a>
 </li>
+`;
+    }).join("")
+}
+
+galleryContainer.insertAdjacentHTML("beforeend",
+    createElementMarkup(galleryItems));
+
+new SimpleLightbox('.gallery a',
+    {
+        captionDelay: 250,
+        captionsData: "alt",
+    }
+);
+
+console.log(galleryItems);
 
 console.log(galleryItems);
